6c699bd2a075e3d9be9cd5798e0bf1618fb9e7e7
ttohaha <108796182+ttohaha@users.noreply.github.com>
2024-07-16 02:48:59 +0300

    try_8

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -32,7 +32,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://192.168.0.107:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
